ed70bcda6bffb7cdf9c27b455a156c7794a180a3
kiwata <iwata.kenaaa@gmail.com>
2020-10-10 21:03:55 +0900

    [AC] hhkb2020 hhkb2020_a

---
+++ atcoder.jp/hhkb2020/hhkb2020_a/Main.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    s = input()
+    t = input()
+
+    if s == 'N':
+        print(t)
+        exit()
+    if t == 'a':
+        print('A')
+    if t == 'b':
+        print('B')
+    if t == 'c':
+        print('C')
+
+
