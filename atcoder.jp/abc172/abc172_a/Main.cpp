1927911fd67be68551dc663466bd3dd058f3672f
kiwata <iwata.kenaaa@gmail.com>
2020-10-13 23:57:06 +0900

    [AC] abc172 abc172_a

---
+++ atcoder.jp/abc172/abc172_a/Main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+
+int main() {
+    int n ;
+    cin >> n;
+    cout << n*n*n + n*n + n << endl;
+}
+
+
