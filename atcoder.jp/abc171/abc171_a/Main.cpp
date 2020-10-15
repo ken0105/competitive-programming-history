07e0a8adbc9073adb9158fa28cafe82cd4220ced
kiwata <iwata.kenaaa@gmail.com>
2020-10-15 22:44:37 +0900

    [AC] abc171 abc171_a

---
+++ atcoder.jp/abc171/abc171_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#define ll long long
+
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    if(a >= "a"){
+        cout << "a" ;
+    }else{
+        cout << "A";
+    }
+}
+
+
