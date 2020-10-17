44d49aa6bcdc8ac8d571dbb262efbd0261c006f8
kiwata <iwata.kenaaa@gmail.com>
2020-10-17 20:01:22 +0900

    [AC] abc180 abc180_a

---
+++ atcoder.jp/abc180/abc180_a/Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sstream>
+
+#define ll long long
+#define rep(i, n) for(int i = 0; i< (int)(n); i++)
+
+using namespace std;
+
+int main() {
+    int n , a, b = 0;
+    cin >> n >> a >> b;
+    cout << n - a + b ;
+}
+
+
+
