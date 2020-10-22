7664d4b2010b70c387162a888d19520e74ed8cc9
kiwata <iwata.kenaaa@gmail.com>
2020-10-22 22:03:53 +0900

    [AC] abc162 abc162_b

---
+++ atcoder.jp/abc162/abc162_b/Main.py
@@ -0,0 +1,11 @@
+import math
+from decimal import *
+import numpy as np
+
+if __name__ == '__main__':
+    n = int(input())
+    cnt = 0
+    for i in range(1,n+1):
+        if i % 3 != 0 and i % 5 != 0:
+            cnt += i
+    print(cnt)
