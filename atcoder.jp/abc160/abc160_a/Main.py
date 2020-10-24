21fa2b7f36228bebf086bc61ac409f2219dbad35
kiwata <iwata.kenaaa@gmail.com>
2020-10-24 23:54:02 +0900

    [AC] abc160 abc160_a

---
+++ atcoder.jp/abc160/abc160_a/Main.py
@@ -0,0 +1,11 @@
+import math
+from decimal import *
+import numpy as np
+
+if __name__ == '__main__':
+    s = input()
+    if s[2] == s[3] and s[4] == s [5]:
+        print('Yes')
+    else:
+        print('No')
+
