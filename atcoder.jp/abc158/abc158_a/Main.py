7fc733cfbf2a2175e9cf7701f87785c9bf4d59c5
kiwata <iwata.kenaaa@gmail.com>
2020-10-26 23:34:30 +0900

    [AC] abc158 abc158_a

---
+++ atcoder.jp/abc158/abc158_a/Main.py
@@ -0,0 +1,13 @@
+import math
+from decimal import *
+import numpy as np
+
+
+if __name__ == '__main__':
+    s = input()
+    if 'A' in s and 'B' in s:
+        print("Yes")
+    else:
+        print("No")
+
+
