5d8e3b0bc04806ef493554c447375a2f202d3a9a
kiwata <iwata.kenaaa@gmail.com>
2020-10-29 23:54:32 +0900

    [AC] abc155 abc155_a

---
+++ atcoder.jp/abc155/abc155_a/Main.py
@@ -0,0 +1,12 @@
+import math
+from decimal import *
+import numpy as np
+
+if __name__ == '__main__':
+    s = set(map(int,input().split()))
+    if len(s) == 2:
+        print("Yes")
+    else:
+        print("No")
+
+
