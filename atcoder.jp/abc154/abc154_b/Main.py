e5f3dfebe16cadaeda4744891840706edef745ad
kiwata <iwata.kenaaa@gmail.com>
2020-10-31 00:02:36 +0900

    [AC] abc154 abc154_b

---
+++ atcoder.jp/abc154/abc154_b/Main.py
@@ -0,0 +1,13 @@
+import math
+from decimal import *
+import numpy as np
+from collections import deque
+
+if __name__ == '__main__':
+    s = input()
+    string = ""
+    for i in range(len(s)):
+        string += "x"
+    print(string)
+
+
