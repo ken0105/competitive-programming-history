0afcab90d5f093dc1d4adebf75b8663e873368ab
kiwata <iwata.kenaaa@gmail.com>
2020-11-03 00:44:04 +0900

    [AC] abc148 abc148_a

---
+++ atcoder.jp/abc148/abc148_a/Main.py
@@ -0,0 +1,16 @@
+import math
+from decimal import *
+import numpy as np
+from collections import deque, Counter
+import itertools
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    s = [1,2,3]
+    s.remove(a)
+    s.remove(b)
+    print(s[0])
+
+
