cc2879e86a34893820ab926cfc97656c89eb4475
kiwata <iwata.kenaaa@gmail.com>
2020-11-04 23:02:11 +0900

    [AC] abc145 abc145_a

---
+++ atcoder.jp/abc145/abc145_a/Main.py
@@ -0,0 +1,10 @@
+import math
+from decimal import *
+import numpy as np
+from collections import deque, Counter
+import itertools
+
+if __name__ == '__main__':
+    r = int(input())
+    print(r ** 2)
+
