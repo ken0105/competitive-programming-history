87b9f8a56706d97704417fa93b5be06d39becdc8
kiwata <iwata.kenaaa@gmail.com>
2020-11-08 21:01:02 +0900

    [AC] abc182 abc182_a

---
+++ atcoder.jp/abc182/abc182_a/Main.py
@@ -0,0 +1,9 @@
+import math
+from decimal import *
+import numpy as np
+from collections import deque, Counter
+import itertools
+
+if __name__ == '__main__':
+    a,b = map(int,input().split())
+    print(2*a + 100 - b)
