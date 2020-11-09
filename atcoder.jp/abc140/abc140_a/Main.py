845522ffa3e8733012ce12f6a3f683cc920d0274
kiwata <iwata.kenaaa@gmail.com>
2020-11-09 23:37:50 +0900

    [AC] abc140 abc140_a

---
+++ atcoder.jp/abc140/abc140_a/Main.py
@@ -0,0 +1,14 @@
+# import math
+# from decimal import *
+# import numpy as np
+# from collections import deque, Counter
+# import itertools
+
+if __name__ == '__main__':
+    n = int(input())
+    print(n ** 3)
+
+
+
+
+
