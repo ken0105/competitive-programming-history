a618ad83072929dd82a7882186071e27256349d1
kiwata <iwata.kenaaa@gmail.com>
2020-11-11 22:27:58 +0900

    [AC] abc138 abc138_a

---
+++ atcoder.jp/abc138/abc138_a/Main.py
@@ -0,0 +1,21 @@
+import math
+# from decimal import *
+# import numpy as np
+# from collections import deque, Counter
+# import itertools
+import sys
+
+if __name__ == '__main__':
+    a = int(input())
+    s = input()
+
+    if a >= 3200: print(s)
+    else: print("red")
+
+
+
+
+
+
+
+
