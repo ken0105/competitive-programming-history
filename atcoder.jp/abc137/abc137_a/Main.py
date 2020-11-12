1423956b98294c1661d2206080a502b940a4ed8e
kiwata <iwata.kenaaa@gmail.com>
2020-11-12 23:40:59 +0900

    [AC] abc137 abc137_a

---
+++ atcoder.jp/abc137/abc137_a/Main.py
@@ -0,0 +1,19 @@
+import math
+# from decimal import *
+# import numpy as np
+# from collections import deque, Counter
+# import itertools
+import sys
+
+if __name__ == '__main__':
+    a,b = map(int, input().split())
+    print(max(max(a+b, a-b),a*b))
+
+
+
+
+
+
+
+
+
