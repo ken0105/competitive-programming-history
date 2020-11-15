4b012d3ec6784d4a2b0b852b618e3be22561fffd
kiwata <iwata.kenaaa@gmail.com>
2020-11-15 21:01:22 +0900

    [AC] abc183 abc183_a

---
+++ atcoder.jp/abc183/abc183_a/Main.py
@@ -0,0 +1,14 @@
+import math
+# from decimal import *
+# import numpy as np
+# from collections import deque, Counter
+# import itertools
+import sys
+import collections
+
+if __name__ == '__main__':
+    x = int(input())
+    if x >= 0:
+        print(x)
+    else:
+        print(0)
