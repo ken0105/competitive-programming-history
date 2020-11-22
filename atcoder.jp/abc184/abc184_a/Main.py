aaa9861cb19bf09bb689e3a95aa0e6ed8eba0c4c
kiwata <iwata.kenaaa@gmail.com>
2020-11-22 21:01:04 +0900

    [AC] abc184 abc184_a

---
+++ atcoder.jp/abc184/abc184_a/Main.py
@@ -0,0 +1,16 @@
+# import math
+# from decimal import *
+# import numpy as np
+
+# from collections import deque, Counter
+# import itertools
+
+
+# import sys
+# import collections
+from collections import deque
+
+if __name__ == '__main__':
+    a,b = map(int,input().split())
+    c,d = map(int,input().split())
+    print(a*d - b*c)
