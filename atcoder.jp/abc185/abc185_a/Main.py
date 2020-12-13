ed9b191751c22639c55b583b58433acb371a982a
kiwata <iwata.kenaaa@gmail.com>
2020-12-13 21:01:06 +0900

    [AC] abc185 abc185_a

---
+++ atcoder.jp/abc185/abc185_a/Main.py
@@ -0,0 +1,15 @@
+# import math
+# # from decimal import *
+# import numpy as np
+#
+# # from collections import deque, Counter
+# import itertools
+#
+# # import sys
+# import collections
+# from collections import deque
+import copy
+
+if __name__ == '__main__':
+    a,b,c,d = map(int,input().split())
+    print(min(a,b,c,d))
