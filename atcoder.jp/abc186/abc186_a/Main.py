fd8dad6491548dfcac08e840f2723c5e3ba2c03a
kiwata <iwata.kenaaa@gmail.com>
2020-12-19 21:01:12 +0900

    [AC] abc186 abc186_a

---
+++ atcoder.jp/abc186/abc186_a/Main.py
@@ -0,0 +1,17 @@
+# import math
+# # from decimal import *
+# import numpy as np
+#
+# # from collections import deque, Counter
+# import itertools
+#
+# # import sys
+# import collections
+# from collections import deque
+# import copy
+
+if __name__ == '__main__':
+    n,w = map(int,input().split())
+    print(n//w)
+
+
