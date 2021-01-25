c949f87c35aa3deaec925f3e9713c7e64ac7df19
kiwata <iwata.kenaaa@gmail.com>
2021-01-25 16:24:06 +0900

    [AC] abc090 abc090_b

---
+++ atcoder.jp/abc090/abc090_b/Main.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    a,b = map(int,input().split())
+    ans = 0
+    for i in range(a,b+1):
+        i = str(i)
+        if i == i[::-1]:
+            ans += 1
+    print(ans)
