c1c46aac116caa532f87ab352069e289113cb739
kiwata <iwata.kenaaa@gmail.com>
2021-01-26 23:05:18 +0900

    [AC] abc049 abc049_b

---
+++ atcoder.jp/abc049/abc049_b/Main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    h,w = map(int,input().split())
+    ans = []
+    for i in range(h):
+        c = input()
+        ans.append(c)
+        ans.append(c)
+    for a in ans:
+        print(a)
