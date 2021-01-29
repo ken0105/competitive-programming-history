a2c511476b8c7c0586fd1bc23c52285acdb5e8ab
kiwata <iwata.kenaaa@gmail.com>
2021-01-29 11:09:45 +0900

    [AC] abc100 abc100_c

---
+++ atcoder.jp/abc100/abc100_c/Main.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    a = list(map(int,input().split()))
+    cnt = 0
+    for i in a:
+        while i % 2 == 0:
+            i //= 2
+            cnt += 1
+    print(cnt)
