1947ac0d339528dc5bd73447d460bc731c4753d5
kiwata <iwata.kenaaa@gmail.com>
2021-01-30 21:03:03 +0900

    [AC] abc190 abc190_a

---
+++ atcoder.jp/abc190/abc190_a/Main.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    a,b,c = map(int,input().split())
+    if c == 0:
+        if a == b:
+            print("Aoki")
+        elif a > b:
+            print("Takahashi")
+        else:
+            print("Aoki")
+    else:
+        if a == b:
+            print("Takahashi")
+        elif a > b:
+            print("Takahashi")
+        else:
+            print("Aoki")
