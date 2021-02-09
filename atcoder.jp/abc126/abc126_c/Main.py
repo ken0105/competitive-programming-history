4362e730bf61c2c41c91e61bbd52fbe562f9421b
kiwata <iwata.kenaaa@gmail.com>
2021-02-09 22:54:04 +0900

    [AC] abc126 abc126_c

---
+++ atcoder.jp/abc126/abc126_c/Main.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    n,k = map(int,input().split())
+    ans = 0
+    for i in range(1,n+1):
+        p = 1 / n
+        while i < k:
+            i *= 2
+            p /= 2
+        ans += p
+    print(ans)
+
