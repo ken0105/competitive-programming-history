32a67c697302a896aace87223c8cb938c7231bef
kiwata <iwata.kenaaa@gmail.com>
2021-03-09 22:32:07 +0900

    [AC] past202005-open past202005_c

---
+++ atcoder.jp/past202005-open/past202005_c/Main.py
@@ -0,0 +1,15 @@
+def main():
+    a,r,n = map(int, input().split())
+    if r == 1:
+        print(a)
+        exit()
+    for i in range(n-1):
+        a *= r
+        if a > 10 ** 9:
+            print("large")
+            exit()
+    print(a)
+
+
+if __name__ == '__main__':
+    main()
